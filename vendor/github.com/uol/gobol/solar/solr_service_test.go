169be889514555a107fdceabf32d5bb963a3f612
Alice <aligomes@uolinc.com>
2018-06-21 14:01:02 -0300

    glide update

--- vendor/github.com/uol/gobol/solar/solr_service_test.go
+++ vendor/github.com/uol/gobol/solar/solr_service_test.go
@@ -1,7 +1,6 @@
-package solar_test
+package solar
 
 import (
-	"errors"
 	"fmt"
 	"io/ioutil"
 	"math/rand"
@@ -20,7 +19,7 @@ import (
 )
 
 const letterBytes = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ"
-const solrUrl = "http://172.17.0.2:8983/solr"
+const solrURL = "http://172.17.0.3:8983/solr"
 
 // getLogger - creates the logger
 func getLogger(t *testing.T) (*zap.Logger, error) {
@@ -49,7 +48,7 @@ func initSolrService(t *testing.T) *SolrService {
 
 	logger, _ := getLogger(t)
 
-	ss, err := NewSolrService(solrUrl, logger)
+	ss, err := NewSolrService(solrURL, logger)
 	if err != nil {
 		t.Errorf(err.Error())
 		panic(err)
@@ -62,7 +61,7 @@ func initSolrService(t *testing.T) *SolrService {
 func createCollection(t *testing.T, ss *SolrService) (string, error) {
 
 	collection := randStringBytes(10)
-	err := ss.CreateCollection(collection, 1, 2)
+	err := ss.CreateCollection(collection, "", 1, 2)
 	if err != nil {
 		t.Errorf(err.Error())
 		return "", err
@@ -88,6 +87,51 @@ func collectionExists(t *testing.T, ss *SolrService, collection string) (bool, e
 	}
 
 	return found, nil
+}
+
+// add a doc to collection
+func populateCollection(t *testing.T, doc solr.Document, ss *SolrService, collection string) error {
+
+	err := ss.AddDocuments(collection, true, doc)
+	if err != nil {
+		t.Errorf(err.Error())
+		return err
+	}
+
+	return nil
+}
+
+func assertDocExistsByID(t *testing.T, collection, id string) {
+	resp, err := http.Get(fmt.Sprintf("%s/%s/select?indent=off&wt=json&q=id:%s", solrURL, collection, id))
+	if err != nil {
+		t.Errorf(err.Error())
+		return
+	}
+
+	bodyString, err := getBodyContent(t, resp)
+	if err != nil {
+		t.Errorf(err.Error())
+		return
+	}
+
+	assert.Contains(t, bodyString, "\"numFound\":1")
+	assert.Contains(t, bodyString, fmt.Sprintf("\"id\":\"%s\"", id))
+}
+
+func assertDocNotExistByQuery(t *testing.T, collection, query string) {
+	resp, err := http.Get(fmt.Sprintf("%s/%s/select?indent=off&wt=json&q=%s", solrURL, collection, query))
+	if err != nil {
+		t.Errorf(err.Error())
+		return
+	}
+
+	bodyString, err := getBodyContent(t, resp)
+	if err != nil {
+		t.Errorf(err.Error())
+		return
+	}
+
+	assert.Contains(t, bodyString, "\"numFound\":0")
 }
 
 func TestCollectionCreation(t *testing.T) {
@@ -161,9 +205,9 @@ func getBodyContent(t *testing.T, resp *http.Response) (string, error) {
 		bodyString := string(bodyBytes)
 
 		return bodyString, nil
-	} else {
-		return "", errors.New(fmt.Sprintf("unexpected status code: %d", resp.StatusCode))
 	}
+
+	return "", fmt.Errorf("unexpected status code: %d", resp.StatusCode)
 }
 
 func TestCollectionSchema(t *testing.T) {
@@ -200,7 +244,7 @@ func TestCollectionSchema(t *testing.T) {
 		return
 	}
 
-	resp, err := http.Get(fmt.Sprintf("%s/%s/schema/fields?indent=off&wt=json", solrUrl, collection))
+	resp, err := http.Get(fmt.Sprintf("%s/%s/schema/fields?indent=off&wt=json", solrURL, collection))
 	if err != nil {
 		t.Errorf(err.Error())
 		return
@@ -238,7 +282,7 @@ func TestAddSingleDocument(t *testing.T) {
 		return
 	}
 
-	resp, err := http.Get(fmt.Sprintf("%s/%s/select?indent=off&wt=json&q=*:*", solrUrl, collection))
+	resp, err := http.Get(fmt.Sprintf("%s/%s/select?indent=off&wt=json&q=*:*", solrURL, collection))
 	if err != nil {
 		t.Errorf(err.Error())
 		return
@@ -276,7 +320,7 @@ func TestAddMultipleDocuments(t *testing.T) {
 		return
 	}
 
-	resp, err := http.Get(fmt.Sprintf("%s/%s/select?indent=off&wt=json&q=*:*", solrUrl, collection))
+	resp, err := http.Get(fmt.Sprintf("%s/%s/select?indent=off&wt=json&q=*:*", solrURL, collection))
 	if err != nil {
 		t.Errorf(err.Error())
 		return
@@ -289,4 +333,130 @@ func TestAddMultipleDocuments(t *testing.T) {
 	}
 
 	assert.Contains(t, bodyString, "\"numFound\":10")
+}
+
+func TestDeleteDocumentByQuery(t *testing.T) {
+	ss := initSolrService(t)
+
+	collection, err := createCollection(t, ss)
+	if err != nil {
+		t.Errorf(err.Error())
+		return
+	}
+
+	found, err := collectionExists(t, ss, collection)
+	if err != nil {
+		t.Errorf(err.Error())
+		return
+	}
+	if !found {
+		t.FailNow()
+	}
+
+	id := "1"
+	name := "test1"
+	doc := solr.Document{
+		"id": id,
+		"name": name,
+	}
+	err = populateCollection(t, doc, ss, collection)
+	if err != nil {
+		t.Errorf(err.Error())
+		return
+	}
+
+	assertDocExistsByID(t, collection, id)
+
+	query := fmt.Sprintf("name:%s", name)
+
+	err = ss.DeleteDocumentByQuery(collection, true, query)
+	if err != nil {
+		t.Errorf(err.Error())
+		return
+	}
+
+	assertDocNotExistByQuery(t, collection, query)
+}
+
+func TestDeleteDocumentByID(t *testing.T) {
+	ss := initSolrService(t)
+
+	collection, err := createCollection(t, ss)
+	if err != nil {
+		t.Errorf(err.Error())
+		return
+	}
+
+	found, err := collectionExists(t, ss, collection)
+	if err != nil {
+		t.Errorf(err.Error())
+		return
+	}
+	if !found {
+		t.FailNow()
+	}
+
+	id := "2"
+	name := "test2"
+	doc := solr.Document{
+		"id": id,
+		"name": name,
+	}
+	err = populateCollection(t, doc, ss, collection)
+	if err != nil {
+		t.Errorf(err.Error())
+		return
+	}
+
+	assertDocExistsByID(t, collection, id)
+
+	err = ss.DeleteDocumentByID(collection, true, id)
+	if err != nil {
+		t.Errorf(err.Error())
+		return
+	}
+
+	query := fmt.Sprintf("id:%s", id)
+	assertDocNotExistByQuery(t, collection, query)
+}
+
+func TestDeleteDocumentByIDCommitFalse(t *testing.T) {
+	ss := initSolrService(t)
+
+	collection, err := createCollection(t, ss)
+	if err != nil {
+		t.Errorf(err.Error())
+		return
+	}
+
+	found, err := collectionExists(t, ss, collection)
+	if err != nil {
+		t.Errorf(err.Error())
+		return
+	}
+	if !found {
+		t.FailNow()
+	}
+
+	id := "3"
+	name := "test3"
+	doc := solr.Document{
+		"id": id,
+		"name": name,
+	}
+	err = populateCollection(t, doc, ss, collection)
+	if err != nil {
+		t.Errorf(err.Error())
+		return
+	}
+
+	assertDocExistsByID(t, collection, id)
+
+	err = ss.DeleteDocumentByID(collection, false, id)
+	if err != nil {
+		t.Errorf(err.Error())
+		return
+	}
+
+	assertDocExistsByID(t, collection, id)
 }
