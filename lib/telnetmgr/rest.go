e2075439f33fb44c8c64efd1713e31c7a00735da
Rafael Nojiri <peo_rnojiri@uolinc.com>
2019-05-10 21:22:04 -0300

    Changing telnet balancing algorithm to notify other nodes of a new balancing process.

--- lib/telnetmgr/rest.go
+++ lib/telnetmgr/rest.go
@@ -6,6 +6,8 @@ import (
 	"sync/atomic"
 
 	"github.com/julienschmidt/httprouter"
+	"go.uber.org/zap"
+	"go.uber.org/zap/zapcore"
 )
 
 //
@@ -13,8 +15,11 @@ import (
 // author: rnojiri
 //
 
-// URI - the uri from the get connections
-const URI string = "node/connections"
+// CountConnsURI - the uri to the get node connections
+const CountConnsURI string = "node/connections"
+
+// HaltConnsURI - the uri to
+const HaltConnsURI string = "node/halt/balancing"
 
 // HTTPHeaderTotalConnections - the header name to set the total tasks number
 const HTTPHeaderTotalConnections string = "X-Total-Connections"
@@ -24,6 +29,29 @@ func (manager *Manager) CountConnections(w http.ResponseWriter, r *http.Request,
 
 	w.Header().Add(HTTPHeaderTotalConnections, fmt.Sprintf("%d", atomic.LoadUint32(&manager.sharedConnectionCounter)))
 	w.WriteHeader(http.StatusOK)
+
+	return
+}
+
+// HaltTelnetBalancingProcess - tells to this node to halt any running balancing process
+func (manager *Manager) HaltTelnetBalancingProcess(w http.ResponseWriter, r *http.Request, ps httprouter.Params) {
+
+	lf := []zapcore.Field{
+		zap.String("package", "telnetmgr"),
+		zap.String("func", "HaltTelnetBalancingProcess"),
+	}
+
+	if atomic.CompareAndSwapUint32(&manager.haltBalancingProcess, 0, 1) {
+
+		manager.logger.Info("halting the telnet connections balancing process", lf...)
+
+		w.WriteHeader(http.StatusOK)
+		return
+	}
+
+	manager.logger.Info("telnet connections balancing process is already halted", lf...)
+
+	w.WriteHeader(http.StatusProcessing)
 
 	return
 }
